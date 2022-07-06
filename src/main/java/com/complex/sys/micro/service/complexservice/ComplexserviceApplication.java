196928848bc1db9feb1754ef4223a2c435c9230c
srinivasaraok <srinivas.softwareprofessional@gmail.com>
2022-07-06 16:14:30 +0530

    initial commit

---
+++ src/main/java/com/complex/sys/micro/service/complexservice/ComplexserviceApplication.java
@@ -0,0 +1,13 @@
+package com.complex.sys.micro.service.complexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/complex/sys/micro/service/complexservice/ComplexserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complex.sys.micro.service.complexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
